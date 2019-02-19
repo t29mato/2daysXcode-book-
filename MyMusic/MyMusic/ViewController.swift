ed39b05cb02f59e10fc0dab2b70f501c3ddf3c77
mato <tomoya.matou@gmail.com>
2019-02-20 00:05:35 +0900

    project file

--- MyMusic/MyMusic/ViewController.swift
+++ MyMusic/MyMusic/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,40 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
+    let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
+    let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
 
+    var cymbalPlayer = AVAudioPlayer()
+    var guitarPlayer = AVAudioPlayer()
+    var backmusicPlayer = AVAudioPlayer()
 
+    @IBAction func cymbal(_ sender: Any) {
+        soundPlayer(player: &cymbalPlayer, path: cymbalPath, count: 0)
+    }
+    
+    @IBAction func guitar(_ sender: Any) {
+        soundPlayer(player: &guitarPlayer, path: guitarPath, count: 0)
+    }
+    
+    @IBAction func play(_ sender: Any) {
+        soundPlayer(player: &backmusicPlayer, path: backmusicPath, count: -1)
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        backmusicPlayer.stop()
+    }
+    
+    fileprivate func soundPlayer(player:inout AVAudioPlayer, path: URL, count: Int) {
+        do {
+            player = try AVAudioPlayer(contentsOf: path, fileTypeHint: nil)
+            player.numberOfLoops = count
+            player.play()
+        } catch {
+            print("エラーが発生しました")
+        }
+    }
+    
 }
 
