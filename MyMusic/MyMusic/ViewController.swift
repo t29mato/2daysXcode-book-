bb48e79325c7af61d59f6b55e2a30689b4ffbea6
mato <tomoya.matou@gmail.com>
2019-02-20 00:04:43 +0900

    leson4 - music app

---
+++ MyMusic/MyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by t2kmato on 2019/02/19.
+//  Copyright © 2019 t2kmato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
