5fdcb8d5d0c739b999c360dcc20747c1d0efaf02
mato <tomoya.matou@gmail.com>
2019-03-04 05:14:25 +0900

    fin 2-5

--- MyCamera/MyCamera/ViewController.swift
+++ MyCamera/MyCamera/ViewController.swift
@@ -17,15 +17,38 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
 
     @IBOutlet weak var pictureImage: UIImageView!
     @IBAction func cameraButtonAction(_ sender: Any) {
+        let alertController = UIAlertController(title: "確認", message: "選択してください", preferredStyle: .actionSheet)
+        
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            print("カメラは利用できます")
-            let imagePickerController = UIImagePickerController()
-            imagePickerController.sourceType = .camera
-            imagePickerController.delegate = self
-            present(imagePickerController, animated: true, completion: nil)
-        } else {
-            print("カメラは利用できません")
+            let cameraAction = UIAlertAction(title: "カメラ", style: .default, handler: { (action:UIAlertAction) in
+                let imagePickerController = UIImagePickerController()
+                imagePickerController.sourceType = .camera
+                imagePickerController.delegate = self
+                self.present(imagePickerController, animated: true, completion: nil)
+
+            })
+            alertController.addAction(cameraAction)
         }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "フォトライブラリー", style: .default, handler: { (action:UIAlertAction) in
+                let imagePickerController = UIImagePickerController()
+                imagePickerController.sourceType = .photoLibrary
+                imagePickerController.delegate = self
+                self.present(imagePickerController, animated: true, completion: nil)
+            })
+            alertController.addAction(photoLibraryAction)
+            
+
+        }
+        
+        
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        alertController.popoverPresentationController?.sourceView = view
+        
+        present(alertController, animated: true, completion: nil)
     }
     @IBAction func SNSButtonAction(_ sender: Any) {
         if let shareImage = pictureImage.image {
